f7919efc9881bc2280a2101a1aabd611ef2d5307
paul <paul@nms.com.au>
2020-12-03 14:37:44 +1100

    initial commit

---
+++ android/app/src/main/kotlin/solutions/sporting/kezza_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package solutions.sporting.kezza_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:kezza_weather/ui/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Kezza Weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Kezza Weather'),
+    );
+  }
+}
+
